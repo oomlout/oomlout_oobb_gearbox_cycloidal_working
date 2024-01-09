847706cd961ea76f145008e72c4b9dc51d1b6697
Oom Lout <aaron@oomlout.com>
2024-01-09 11:06:53 +0000

    working

--- working.py
+++ working.py
@@ -40,7 +40,8 @@ def make_scad(**kwargs):
 
 
     #make_scad_drive_shaft_input(**kwargs)
-    make_scad_drive_shaft_output_inner(**kwargs)
+    #make_scad_drive_shaft_output_inner(**kwargs)
+    make_scad_drive_shaft_output_outer(**kwargs)
     #make_scad_inner_rotor(**kwargs)
     #kwargs["thickness"] = 12
     #make_scad_outer_rotor(**kwargs)
@@ -86,7 +87,46 @@ def make_scad_drive_shaft_input(**kwargs):
 
 def make_scad_drive_shaft_output_inner(**kwargs):
     
-    kwargs["type"] = "oomlout_oobb_gearbox_cycloidal_working_drive_shaft_input"
+    kwargs["type"] = "oomlout_oobb_gearbox_cycloidal_working_drive_shaft_output_inner"
+     # default sets
+    width = kwargs.get("width", 3)
+    height = kwargs.get("height", 5)
+    thickness = kwargs.get("thickness", 3)
+    size = kwargs.get("size", "oobb")
+    pos = kwargs.get("pos", [0, 0, 0])
+    # extra scts
+    holes = kwargs.get("holes", True)
+    both_holes = kwargs.get("both_holes", True)    
+    kwargs["pos"] = pos
+    
+        # get the default thing
+    thing = oobb_base.get_default_thing(**kwargs)
+    th = thing["components"]
+    kwargs.pop("size","")
+
+    p3 = copy.deepcopy(kwargs)
+    p3["center_offset"] = False
+    get_drive_shaft_input(thing, **kwargs)
+
+    save_type = kwargs.get("save_type", "all")
+    overwrite = True
+    modes = ["3dpr", "laser", "true"]
+    for mode in modes:
+        depth = thing.get(
+            "depth_mm", thing.get("thickness_mm", 3))
+        height = thing.get("height_mm", 100)
+        layers = depth / 3
+        tilediff = height + 10
+        start = 1.5
+        if layers != 1:
+            start = 1.5 - (layers / 2)*3
+        if "bunting" in thing:
+            start = 0.5
+        opsc.opsc_make_object(f'scad_output/{thing["id"]}/{mode}.scad', thing["components"], mode=mode, save_type=save_type, overwrite=overwrite, layers=layers, tilediff=tilediff, start=start)
+
+def make_scad_drive_shaft_output_outer(**kwargs):
+    
+    kwargs["type"] = "oomlout_oobb_gearbox_cycloidal_working_drive_shaft_output_outer"
      # default sets
     width = kwargs.get("width", 3)
     height = kwargs.get("height", 5)
@@ -103,9 +143,7 @@ def make_scad_drive_shaft_output_inner(**kwargs):
     th = thing["components"]
     kwargs.pop("size","")
 
-    p3 = copy.deepcopy(kwargs)
-    p3["center_offset"] = False
-    get_drive_shaft_input(thing, **p3)    
+    get_drive_shaft_output_outer(thing, **kwargs)    
 
     save_type = kwargs.get("save_type", "all")
     overwrite = True
@@ -287,9 +325,150 @@ def get_drive_shaft_input(thing, **kwargs):
     p3 = copy.deepcopy(kwargs)
     p3["type"] = "n"
     p3["shape"] = f"oobb_slice"
-    p3["m"] = "#"
+    #p3["m"] = "#"
     oobb_base.append_full(thing,**p3)
     
+def get_drive_shaft_output_outer(thing, **kwargs):
+
+
+    pos = kwargs.get("pos", [0, 0, 0])
+    #pos = copy.deepcopy(pos)
+    #pos[2] += -20
+    depth = 8
+
+    #add _cylinder
+    p3 = copy.deepcopy(kwargs)
+    p3["type"] = "p"
+    p3["shape"] = f"oobb_cylinder"
+    p3["radius"] = 55/2
+    p3["depth"] = depth
+    #p3["m"] = "#"
+    pos1 = copy.deepcopy(pos)         
+    p3["pos"] = pos1
+    oobb_base.append_full(thing,**p3)
+
+    #add_bearing
+    p3 = copy.deepcopy(kwargs)
+    p3["type"] = "n"
+    p3["shape"] = f"oobb_bearing"
+    p3["bearing"] = "6810"
+    pos1 = copy.deepcopy(pos)      
+    p3["pos"] = pos1
+    #p3["m"] = "#"
+    oobb_base.append_full(thing,**p3)
+
+    #add_bearing
+    p3 = copy.deepcopy(kwargs)
+    p3["type"] = "n"
+    p3["shape"] = f"oobb_bearing"
+    p3["bearing"] = "6705"
+    pos1 = copy.deepcopy(pos)      
+    p3["pos"] = pos1
+    oobb_base.append_full(thing,**p3)
+
+
+    
+    #add joining holes
+    p3 = copy.deepcopy(kwargs)
+    p3["type"] = "n"
+    p3["shape"] = f"oobb_screw_countersunk"
+    p3["radius_name"] = "m3"
+    p3["depth"] = depth
+    p3["nut_include"] = True
+    p3["zz"] = "bottom"
+    poss = []
+    if True:
+        offset = 20
+        pos1 = copy.deepcopy(pos)
+        pos1[2] += -depth/2    
+        pos13 = copy.deepcopy(pos1)
+        pos13[1] += offset
+        poss.append(pos13)
+        pos14 = copy.deepcopy(pos1)
+        pos14[1] += -offset
+        poss.append(pos14)
+    p3["pos"] = poss
+    #p3["m"] = "#"
+    oobb_base.append_full(thing,**p3)
+
+    p3 = copy.deepcopy(kwargs)
+    p3["type"] = "n"
+    p3["shape"] = f"oobb_screw_countersunk"
+    p3["radius_name"] = "m3"
+    p3["depth"] = depth
+    p3["nut_include"] = True
+    p3["zz"] = "top"
+    p3["rot"] = [0,180,0]
+    poss = []
+    if True:
+        offset = 20
+        pos1 = copy.deepcopy(pos)
+        pos1[2] += -depth/2    
+        pos11 = copy.deepcopy(pos1)
+        #do all four permutations
+        pos11[0] += offset
+        poss.append(pos11)
+        pos12 = copy.deepcopy(pos1)
+        pos12[0] += -offset
+        poss.append(pos12)
+    p3["pos"] = poss
+    #p3["m"] = "#"
+    oobb_base.append_full(thing,**p3)
+
+    
+    #put into a rotation object
+    components_second = copy.deepcopy(thing["components"])
+    return_value_2 = {}
+    return_value_2["type"]  = "rotation"
+    return_value_2["typetype"]  = "p"
+    pos1 = copy.deepcopy(pos)
+    pos1[0] += 100
+    return_value_2["pos"] = pos1
+    return_value_2["rot"] = [180,0,0]
+    return_value_2["objects"] = components_second
+    
+    thing["components"].append(return_value_2)
+
+
+    #add slice # top
+    p3 = copy.deepcopy(kwargs)
+    p3["type"] = "n"
+    p3["shape"] = f"oobb_slice"
+    #p3["m"] = "#"
+    oobb_base.append_full(thing,**p3)
+    
+
+
+    #add oobb holes
+    p3 = copy.deepcopy(kwargs)
+    p3["type"] = "n"
+    p3["shape"] = f"oobb_hole"
+    p3["radius_name"] = "m3"
+    p3["depth"] = depth
+    poss = []
+    if True:
+        offset = 15.556        
+        pos1 = copy.deepcopy(pos)
+        pos1[2] += -depth/2
+        pos11 = copy.deepcopy(pos1)
+        pos11[0] += offset
+        pos11[1] += offset
+        poss.append(pos11)
+        pos12 = copy.deepcopy(pos1)
+        pos12[0] += -offset
+        pos12[1] += offset
+        poss.append(pos12)
+        pos13 = copy.deepcopy(pos1)
+        pos13[0] += offset
+        pos13[1] += -offset
+        poss.append(pos13)
+        pos14 = copy.deepcopy(pos1)
+        pos14[0] += -offset
+        pos14[1] += -offset
+        poss.append(pos14)
+    p3["pos"] = poss
+    #p3["m"] = "#"
+    oobb_base.append_full(thing,**p3)
 
 def make_scad_full(**kwargs):
   
@@ -510,9 +689,10 @@ def get_inner_rotor(thing, **kwargs):
     lobe_number = kwargs.get("lobe_number", 6)
     radius_offset = kwargs.get("radius_offset", 1.5)
     radius_pin = 6/2
-    depth = 8
+    depth = 6
 
-    output_shaft_pin_distance = 60
+    #output_shaft_pin_distance = 60
+    output_shaft_pin_distance = 44
 
     #add _cycloid
     p3 = copy.deepcopy(kwargs)
@@ -521,7 +701,7 @@ def get_inner_rotor(thing, **kwargs):
     p3["lobe_number"] = lobe_number
     p3["radius_offset"] = 1.5
     p3["radius_pin"] = 6/2
-    p3["offset"] = -0.5
+    p3["offset"] = -0.25
     p3["depth"] = depth
     #p3["m"] = "#"
     pos1 = copy.deepcopy(pos) 
@@ -538,29 +718,43 @@ def get_inner_rotor(thing, **kwargs):
     p3["pos"] = pos1
     oobb_base.append_full(thing,**p3)
 
+    bearing = False
+
     #add output shaft bearings
-    p3 = copy.deepcopy(kwargs)
-    p3["type"] = "n"
-    p3["shape"] = f"oobb_bearing"
-    p3["bearing"] = "606"
-    poss = []
-    pos1 = copy.deepcopy(pos)    
-    pos1[2] += 0
-    pos11 = copy.deepcopy(pos1)
-    pos11[0] += output_shaft_pin_distance/2
-    pos12 = copy.deepcopy(pos1)
-    pos12[0] += -output_shaft_pin_distance/2
-    pos13 = copy.deepcopy(pos1)
-    pos13[1] += output_shaft_pin_distance/2
-    pos14 = copy.deepcopy(pos1)
-    pos14[1] += -output_shaft_pin_distance/2
-    poss.append(pos11)
-    poss.append(pos12)
-    poss.append(pos13)
-    poss.append(pos14)
-    p3["pos"] = poss
-    #p3["m"] = "#"
-    oobb_base.append_full(thing,**p3)
+    if True:
+        poss = []
+        pos1 = copy.deepcopy(pos)    
+        pos1[2] += 0
+        pos11 = copy.deepcopy(pos1)
+        pos11[0] += output_shaft_pin_distance/2
+        pos12 = copy.deepcopy(pos1)
+        pos12[0] += -output_shaft_pin_distance/2
+        pos13 = copy.deepcopy(pos1)
+        pos13[1] += output_shaft_pin_distance/2
+        pos14 = copy.deepcopy(pos1)
+        pos14[1] += -output_shaft_pin_distance/2
+        poss.append(pos11)
+        poss.append(pos12)
+        poss.append(pos13)
+        poss.append(pos14)
+        
+    if bearing:
+        p3 = copy.deepcopy(kwargs)
+        p3["type"] = "n"
+        p3["shape"] = f"oobb_bearing"
+        #p3["bearing"] = "606"
+        p3["bearing"] = "676"
+        p3["pos"] = poss
+        #p3["m"] = "#"
+        oobb_base.append_full(thing,**p3)
+    else:
+        p3 = copy.deepcopy(kwargs)
+        p3["type"] = "n"
+        p3["shape"] = f"oobb_hole"        
+        p3["radius"] = 3
+        p3["pos"] = poss
+        #p3["m"] = "#"
+        oobb_base.append_full(thing,**p3)
 
     #add connecting screws
     offset = 15
@@ -586,7 +780,7 @@ def get_inner_rotor(thing, **kwargs):
     p3["pos"] = poss
     p3["zz"] = "bottom"
     p3["overhang"] = True
-    p3["m"] = "#"
+    #p3["m"] = "#"
     oobb_base.append_full(thing,**p3)
 
     #ro 180 on y and do on y
@@ -607,7 +801,7 @@ def get_inner_rotor(thing, **kwargs):
         poss.append(pos11)
         poss.append(pos12)
     p3["pos"] = poss
-    p3["m"] = "#"
+    #p3["m"] = "#"
     oobb_base.append_full(thing,**p3)
 
 
